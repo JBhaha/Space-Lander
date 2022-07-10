82200279fff7db469e774ba8a0f9158c0b3cd3c8
JBhaha <jakus.korona@gmail.com>
2022-07-11 00:15:51 +0200

    game logic implemented

--- src/App.js
+++ src/App.js
@@ -1,84 +1,120 @@
-import React, { useRef, useEffect } from 'react';
+import './App.css';
+
+let gamePiece;
+let obstacle;
+let obstacle2;
+let obstacle3;
+let obstacle4;
+let obstacle5;
+let ctx;
+
+function startGame(){
+  gamePiece = new component(30, 30, "red", 10, 120);
+  obstacle = new component(400, 10, "white", 0, 800);
+  obstacle2 = new component(400, 10, "white", 400, 950);
+  obstacle3 = new component(400, 10, "white", 800, 750);
+  obstacle4 = new component(400, 10, "white", 1200, 1000);
+  obstacle5 = new component(400, 10, "white", 1600, 1100);
+  gameArea.start();
+}
+
+let gameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 2000;
+    this.canvas.height = 1200;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+    window.addEventListener('keydown', function (e) {
+      gameArea.key = e.keyCode;
+    })
+    window.addEventListener('keyup', function (e) {
+      gameArea.key = false;
+    })
+  },
+  clear : function(){
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop : function() {
+    clearInterval(this.interval);
+  }
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.gravity = 0.05;
+  this.gravitySpeed = 0;
+  this.update = function(){
+    ctx = gameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  this.newPos = function() {
+    this.gravitySpeed += this.gravity;
+    this.x += this.speedX;
+    this.y += this.speedY + this.gravitySpeed;
+  }
+
+  this.crashWith = function(otherobj) {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + (otherobj.width);
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + (otherobj.height);
+    var crash = true;
+    if ((mybottom < othertop) ||
+        (mytop > otherbottom) ||
+        (myright < otherleft) ||
+        (myleft > otherright)) {
+      crash = false;
+    }
+    return crash;
+  }
+
+}
+
+function updateGameArea() {
+  if (gamePiece.crashWith(obstacle) && gamePiece.speedY < 0){
+    gameArea.stop();
+  } else {
+    gameArea.clear();
+    gamePiece.speedX = 0;
+    gamePiece.speedY = 0;
+    if (gameArea.key && gameArea.key == 37) {gamePiece.speedX = -4; }
+    if (gameArea.key && gameArea.key == 39) {gamePiece.speedX = 4; }
+    if (gameArea.key && gameArea.key == 38) {accelerate(-1.5); }
+    if (gameArea.key && gameArea.key == 40) {gamePiece.speedY = 1; }
+    gamePiece.newPos();
+    gamePiece.update();
+    obstacle.update();
+    obstacle2.update();
+    obstacle3.update();
+    obstacle4.update();
+    obstacle5.update();
+  }
+
+  function accelerate(n) {
+    gamePiece.speedY = n;
+    gamePiece.gravity = 0;
+  }
+
+}
 
 function App() {
-  const canvas = useRef();
-  let ctx = null;
-  const rocket = useRef();
-
-  // initialize the canvas context
-  useEffect(() => {
-    // dynamically assign the width and height to canvas
-    const canvasEle = canvas.current;
-    canvasEle.width = canvasEle.clientWidth;
-    canvasEle.height = canvasEle.clientHeight;
-
-    // get context of the canvas
-    ctx = canvasEle.getContext('2d');
-  }, []);
-
-  useEffect(() => {
-    drawLine({ x: 0, y: 600, x1: 226, y1: 331 }, { color: 'white' });
-    drawLine({ x: 226, y: 331, x1: 333, y1: 454 }, { color: 'white' });
-    drawLine({ x: 333, y: 454, x1: 592, y1: 454 }, { color: 'white' });
-    drawLine({ x: 592, y: 454, x1: 739, y1: 257 }, { color: 'white' });
-    drawLine({ x: 739, y: 257, x1: 848, y1: 333 }, { color: 'white' });
-    drawLine({ x: 848, y: 333, x1: 998, y1: 136 }, { color: 'white' });
-    drawLine({ x: 998, y: 136, x1: 1164, y1: 136 }, { color: 'white' });
-    drawLine({ x: 1164, y: 136, x1: 1450, y1: 368 }, { color: 'white' });
-  }, []);
-
-  useEffect(() => {
-    let xValue = 0;
-    let yValue = 0;
-
-    window.addEventListener('keydown', (event) => {
-      switch (event.key) {
-        case 'ArrowUp':
-          xValue++;
-          break;
-        case 'ArrowDown':
-          xValue--;
-          break;
-        case 'ArrowLeft':
-          yValue++;
-          break;
-        case 'ArrowRight':
-          yValue--;
-          break;
-      }
-    });
-
-    drawRocket({ x: xValue, y: yValue, w: 30, h: 30 });
-  }, []);
-
-  // draw a line
-  const drawLine = (info, style = {}) => {
-    const { x, y, x1, y1 } = info;
-    const { color = 'black' } = style;
-
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x1, y1);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 3;
-    ctx.stroke();
-  };
-
-  const drawRocket = (info = {}) => {
-    const { x, y, w, h } = info;
-
-    ctx.beginPath();
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 3;
-    ctx.rect(x, y, w, h);
-    ctx.stroke();
-  };
-
+  startGame()
   return (
-    <div className="App">
-      <h3>Space Lander</h3>
-      <canvas ref={canvas}></canvas>
-    </div>
+      <body>
+
+      </body>
   );
 }
 
